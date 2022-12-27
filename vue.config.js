64e2c395159f6f38dbcf5ffe94272dd380440383
Preina Mariya <m.preina@idaproject.com>
2022-12-27 11:57:23 +0400

    fix: small fixes

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,16 @@
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
-  transpileDependencies: true,
-  css: {
-    loaderOptions: {
-      sass: {
-        additionalData: `
+    transpileDependencies: true,
+    css: {
+        loaderOptions: {
+            sass: {
+                additionalData: `
           @import "src/assets/scss/shared/_variables.scss";
           @import "src/assets/scss/shared/_mixins.scss";
           @import "src/assets/scss/shared/_functions.scss";
-        `
-      }
+        `,
+            },
+        },
     },
-  },
-  publicPath: process.env.NODE_ENV === 'production' ? '/tree/' : '/'
+    publicPath: process.env.NODE_ENV === 'production' ? '/tree/' : '/',
 });
